07d9f6ab713613c5fbdb11e24e4bdf8bd027c09d
Necrobone <necrobone@hotmail.com>
2020-02-21 20:08:28 +0100

    Deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,38 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
 import { ComponentModule } from './component/component.module';
 
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
-    imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule, ComponentModule],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        ComponentModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+    ],
     providers: [
         StatusBar,
         SplashScreen,
         {
             provide: RouteReuseStrategy,
-            useClass: IonicRouteStrategy
-        }
+            useClass: IonicRouteStrategy,
+        },
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
 })
 export class AppModule {
 }
